3648f8bb0053362b9d083c66e0e321c7bca63183
Siddharth Verma <>
2025-05-20 13:44:03 +0300

    Fixed the camera passing through the walls, updated hierarchy of the camera

---
+++ Assets/Game/Engine/PlayerCameraController.cs
@@ -0,0 +1,50 @@
+using UnityEngine;
+using UnityEngine.Serialization;
+
+namespace Game.Engine
+{
+    public class PlayerCameraController : MonoBehaviour
+    {
+        [SerializeField] private Transform target;
+
+        [SerializeField] private Vector3 offset;
+        [SerializeField] private float followSpeed;
+        [SerializeField] private float rotSpeed;
+
+        [SerializeField] private Transform dollyCamera;
+        
+        private float _distanceFromTarget;
+        private Vector3 _normalisedDirection;
+
+        [SerializeField] private float minDistance;
+
+        private void Update()
+        {
+            Vector3 toPosition = target.TransformPoint(offset);
+            _normalisedDirection = (toPosition - target.position).normalized;
+
+            if (Physics.Linecast(target.position, transform.position, out RaycastHit hit) &&
+                !hit.transform.name.Equals("Hero"))
+            {
+                _distanceFromTarget = Mathf.Clamp(hit.distance * 0.65f, minDistance, 15f);
+            }
+            else
+            {
+                _distanceFromTarget = Vector3.Distance(toPosition, target.position);
+            }
+
+            Vector3 finalPosition = target.position + (_normalisedDirection * _distanceFromTarget);
+            dollyCamera.position = Vector3.Lerp(dollyCamera.position, finalPosition, Time.deltaTime * 5f);
+        }
+
+        private void LateUpdate()
+        {
+            Vector3 toPosition = target.TransformPoint(offset);
+            transform.position = toPosition;
+
+            Vector3 targetRot = target.rotation.eulerAngles;
+            float xRotation = transform.rotation.eulerAngles.x;
+            transform.rotation = Quaternion.Euler(xRotation, targetRot.y, 0);
+        }
+    }
+}

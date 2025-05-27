58f16695d47edcf973fd07df18f5d7992913831b
Siddharth Verma <>
2025-05-27 23:16:12 +0300

    cleanup

--- Assets/Game/Engine/Movement/HeroAnimatorController.cs
+++ Assets/Game/Engine/Movement/HeroAnimatorController.cs
@@ -4,20 +4,16 @@ namespace Game.Engine.Movement
 {
     public class HeroAnimatorController : MonoBehaviour
     {
+        private static readonly int Move = Animator.StringToHash("Move");
+        private static readonly int Jump = Animator.StringToHash("Jump");
+        
         [SerializeField] private Animator animator;
         
         public void LogData(Vector3 moveDirection, Vector3 velocity, float velocityY)
         {
-            // Debug.Log($"LogData : Dir: {moveDirection}, Velocity: {velocity}, yVelocity: {velocityY}");
-
-            float tt =  new Vector2(velocity.x, velocity.z).magnitude;
-            
-            // Debug.Log(Mathf.FloorToInt(tt));
-            
-            animator.SetInteger("Move", Mathf.FloorToInt(tt));
-            animator.SetInteger("Jump", Mathf.FloorToInt(velocity.y));
-            // animator.SetFloat("vY", velocity.y);
-            // animator.SetFloat("vZ", velocity.z);
+            float traversal =  new Vector2(velocity.x, velocity.z).magnitude;
+            animator.SetInteger(Move, Mathf.FloorToInt(traversal));
+            animator.SetInteger(Jump, Mathf.FloorToInt(velocity.y));
         }
     }
 }
